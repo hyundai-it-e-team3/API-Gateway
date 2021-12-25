6557a2737273a4478d24ebae4b0ff30f6d83f019
jungwoo <kjw970103@gmail.com>
2021-12-25 21:32:46 +0900

    HYUN-241 gateway jwt 설정 추가

---
+++ src/main/java/com/mycompany/gatewayservice/security/JwtUtil.java
@@ -0,0 +1,75 @@
+package com.mycompany.gatewayservice.security;
+
+import java.util.Date;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JwtUtil {
+	//비밀키
+	private static final String secretKey = "12345";
+	
+	//JWT 생성
+	public static String createToken(String memberId, String authority) {
+		log.info("Run");
+		
+		String result = null;
+		
+		try {
+			String token = Jwts.builder()
+					//헤더 설정(토큰 타입, 알고리즘 종류)
+					.setHeaderParam("typ", "JWT")
+					.setHeaderParam("alg", "HS256")
+					//토근의 유효기간 설정
+					.setExpiration(new Date(new Date().getTime() + 1000*60*60*24)) //1000=1초
+					//페이로드or크레임(토큰에 저장되는 데이터) 설정
+					.claim("memberId", memberId)
+					.claim("authority", authority)
+					//서명 설정
+					.signWith(SignatureAlgorithm.HS256, secretKey.getBytes("UTF-8"))
+					.compact();
+			result = token;	
+		} catch(Exception e) {		
+		}
+		
+		return result;
+	}
+	
+	//JWT 유효성 검사
+	public static Claims validateToken(String token) {
+		log.info("Run");
+		
+		Claims result = null;
+		
+		try {
+			Claims claims = Jwts.parser()
+					.setSigningKey(secretKey.getBytes("UTF-8"))
+					.parseClaimsJws(token) //파싱 예외 발생 구간
+					.getBody();
+			
+			result = claims;
+		} catch(Exception e) {
+		}
+		
+		return result;
+		
+	}
+	
+	//JWT 정보 얻기
+	public static String getMemberId(Claims claims) {
+		log.info("Run");
+		
+		String memberId = claims.get("memberId", String.class);
+		
+		return memberId;
+	}
+	
+	public static String getAuthority(Claims claims) {
+		log.info("Run");
+		
+		return claims.get("authority", String.class);
+	}
+}
